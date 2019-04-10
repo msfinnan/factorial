577de3a9224357ec1ed5b5a9c7cdd17e8686be2c
Margaret Finnan <msfinnan@gmail.com>
2019-04-09 20:14:59 -0700

    worked out new method

--- lib/factorial.rb
+++ lib/factorial.rb
@@ -1,18 +1,54 @@
 # Computes factorial of the input number and returns it
-# Time complexity: O(n), where n is the number passed into the method. 
-# Space complexity: Constant, O(1), the only extra space used is for the variable x 
+# Time complexity: Linear or O(n) where n is the number input 
+# Space complexity: Constant or O(1), will have the same  number of variables no matter the size of the input 
 
 def factorial(number)
-  if number == nil
-    raise ArgumentError, "Not a valid number"
-  elsif number == 0 || number == 1
-    return 1
-  else
-    x = number - 1
-    x.times do 
-      number = number * x
-      x -= 1
-    end
-    return number
-  end
+return 1 if number == 0 
+if number == nil 
+  raise ArgumentError, "Enter an integer"
+end 
+a = 1
+b = number 
+while a < number
+  b = b * (number - a)
+  a += 1
+end 
+return b 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
